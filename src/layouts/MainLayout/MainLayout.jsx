698461da07c7b171789f0aa9c9407cfc2b65c467
Ankon Chowdhury <ankonchy1@gmail.com>
2025-02-24 00:32:34 +0600

    footer added successfully

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { ToastContainer } from "react-toastify";
 import Navbar from "../../components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar />
       <Outlet></Outlet>
+      <Footer></Footer>
       <ToastContainer />
     </div>
   );
